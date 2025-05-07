44f3df90a6888b603ca099e18558d67c7a4105f5
Ramos223 <luisenriqueramos223@gmail.com>
2025-05-07 13:01:46 -0600

    First commit

---
+++ src/test/java/com/tecdesoftware/market_app_3c/MarketApp3cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.market_app_3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApp3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
